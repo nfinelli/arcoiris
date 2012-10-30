e2ad4010424c59ac2336e1108bdfca16295b572f
nfinelli <njfinelli@hotmail.com>
2012-10-30 01:16:44 -0300

    Test 4

--- sistema/src/bo/Entrega.java
+++ sistema/src/bo/Entrega.java
@@ -4,9 +4,9 @@ public class Entrega extends PersistentObject {
 	
 	private String estado; 	// ENTREGADA / PENDIENTE DE ENTREGA
 	
-	private Venta venta;
+	private Venta venta; 
 	
-	private Double costo;
+	private Double costo; 
 	
 	private String fletero;
 	
