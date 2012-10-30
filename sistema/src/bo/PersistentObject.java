90410c94f21f240cf420c97adbbd0bc5edaef4f1
nfinelli <njfinelli@hotmail.com>
2012-10-30 00:34:31 -0300

    Archivo de Proyecto

--- sistema/src/bo/PersistentObject.java
+++ sistema/src/bo/PersistentObject.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 public class PersistentObject implements Serializable {
 
+	// TODO hacer que ID se genere automaticamente y tenga un prefijo segun el local
 	private Long id;
 	
 	private Date timestamp;
